0b19f9c59cad411e25296b751739acedf8c54a56
Jonas Gierer <jonas@gierer.xyz>
2022-09-10 16:04:08 +0200

    Remove Astro specific config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,4 @@
 module.exports = {
 	singleQuote: true,
 	useTabs: true,
-
-	plugins: [require.resolve("prettier-plugin-astro")],
-	overrides: [
-		{
-			files: "*.astro",
-			options: {
-				parser: "astro",
-			},
-		},
-	],
 };
