f64c4e48ca8e469c98a1f4369dd557abca9e7238
Jonas Gierer <jonas@gierer.xyz>
2022-09-10 15:59:15 +0200

    Init

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+	singleQuote: true,
+	useTabs: true,
+
+	plugins: [require.resolve("prettier-plugin-astro")],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+Some templates for files I commonly create.
+
+Folders contain templates for specific project types.
